1b72875f66086a46b7b05b7106ae0879b32f2c3a
Jesse Silva <jesse1eliseu@gmail.com>
2025-06-11 20:47:36 -0300

    feat: Add Android Gradle configuration and update dependencies
    - Created `capacitor.settings.gradle` for Capacitor Android project configuration.
    - Added `gradle.properties` to define project-wide Gradle settings.
    - Included `gradle-wrapper.jar` and `gradle-wrapper.properties` for Gradle wrapper setup.
    - Implemented `gradlew` and `gradlew.bat` scripts for executing Gradle tasks.
    - Updated `settings.gradle` to include Capacitor and Cordova plugins.
    - Defined Android SDK versions and dependencies in `variables.gradle`.
    - Updated Angular project configuration in `angular.json` to disable analytics.
    - Updated package dependencies in `package-lock.json` and `package.json` for Angular, Ionic, and Capacitor.
    - Implemented user authentication and vehicle management features in the app.
    - Created new components and models for vehicle management.
    - Updated home page to include user authentication form.
    - Added vehicle listing and deletion functionality in the vehicle page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'veiculo',
+    loadComponent: () => import('./veiculo/veiculo.page').then( m => m.VeiculoPage)
+  },
 ];
